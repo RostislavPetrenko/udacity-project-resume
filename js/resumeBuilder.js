d6d50b879f6dcd6874c6b16a7903a10dd2275776
Richard Kalehoff <richardkalehoff@gmail.com>
2017-04-06 11:09:59 -0700

    update resume objects w/ more info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,16 @@
 const bio = {
-    name: 'Richard',
+    name: 'Richard Kalehoff',
     role: 'Senior Content Developer',
     contacts: {
         mobile: '954-123-4567',
         email: 'richardkalehoff@gmail.com',
-        github: 'https://github.com/richardkalehoff',
-        twitter: 'https://twitter.com/richardkalehoff',
+        github: 'richardkalehoff',
+        twitter: '@richardkalehoff',
         location: 'Mountain View',
     },
     welcomeMessage: 'Howdy',
     skills: ['HTML', 'CSS', 'JavaScript'],
-    biopic: 'url',
+    biopic: '../images/fry.jpg',
     display: function () {},
 };
 
@@ -51,21 +51,21 @@ const work = {
             title: 'Senior Web Engineer',
             location: 'Fort Lauderdale, FL',
             dates: '2008-2015',
-            description: ''
+            description: 'Cupcake ipsum dolor. Sit amet candy canes chupa chups apple pie tart oat cake lollipop. Croissant tiramisu cupcake.'
         },
         {
             employer: 'Udacity',
             title: 'Course Developer',
             location: 'Mountain View, CA',
             dates: '2015-2016',
-            description: ''
+            description: 'Powder soufflé sesame snaps cookie cheesecake macaroon muffin marzipan. Dessert gummies sesame snaps muffin donut cupcake jelly sweet lollipop. Pastry jelly-o gingerbread cake marzipan donut cake cheesecake. Sesame snaps soufflé bonbon pie.'
         },
         {
             employer: 'Udacity',
             title: 'Senior Content Developer',
             location: 'Mountain View, CA',
             dates: '2016-present',
-            description: ''
+            description: 'Gummies tootsie roll candy canes donut. Tart tiramisu pie apple pie. Biscuit pie candy cotton candy sweet sugar plum bear claw. Fruitcake candy dessert marshmallow gummi bears.'
         },
     ],
     display: function () {},
@@ -74,10 +74,16 @@ const work = {
 const projects = {
     projects: [
         {
-            title: '',
-            dates: '2015-2016',
-            description: '',
-            images: ['']
+            title: 'Sample Project 1',
+            dates: '2009-2009',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            images: ['../images/197x148.gif']
+        },
+        {
+            title: '2nd Project',
+            dates: '2013-2014',
+            description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            images: ['../images/197x148.gif']
         }
     ],
     display: function () {},
