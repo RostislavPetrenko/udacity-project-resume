171ef3b29f9f28eac36dd7bd35dac524832781af
Richard Kalehoff <richardkalehoff@gmail.com>
2017-04-05 21:37:53 -0700

    add JS objects with resume info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,84 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+const bio = {
+    name: 'Richard',
+    role: 'Senior Content Developer',
+    contacts: {
+        mobile: '954-123-4567',
+        email: 'richardkalehoff@gmail.com',
+        github: 'https://github.com/richardkalehoff',
+        twitter: 'https://twitter.com/richardkalehoff',
+        location: 'Mountain View',
+    },
+    welcomeMessage: 'Howdy',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    biopic: 'url',
+    display: function () {},
+};
+
+const education = {
+    schools: [
+        {
+            name: 'Belhaven University',
+            location: 'Jackson, MS',
+            degree: 'Bachelor of Science',
+            majors: ['Computer Science'],
+            dates: '2003-2007',
+            url: 'http://www.belhaven.edu/',
+        },
+        {
+            name: 'University of Florida',
+            location: 'Gainesville, FL',
+            degree: 'Master of Arts',
+            majors: ['Mass Communications'],
+            dates: '2013-2014',
+            url: 'http://www.ufl.edu/'
+        },
+    ],
+    onlineCourses: [
+        {
+            title: 'Front-End Web Developer Nanodegree',
+            school: 'Udacity',
+            dates: '2016-2017',
+            url: 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001'
+        }
+    ],
+    display: function () {},
+};
+
+const work = {
+    jobs: [
+        {
+            employer: 'EE International',
+            title: 'Senior Web Engineer',
+            location: 'Fort Lauderdale, FL',
+            dates: '2008-2015',
+            description: ''
+        },
+        {
+            employer: 'Udacity',
+            title: 'Course Developer',
+            location: 'Mountain View, CA',
+            dates: '2015-2016',
+            description: ''
+        },
+        {
+            employer: 'Udacity',
+            title: 'Senior Content Developer',
+            location: 'Mountain View, CA',
+            dates: '2016-present',
+            description: ''
+        },
+    ],
+    display: function () {},
+};
+
+const projects = {
+    projects: [
+        {
+            title: '',
+            dates: '2015-2016',
+            description: '',
+            images: ['']
+        }
+    ],
+    display: function () {},
+};
